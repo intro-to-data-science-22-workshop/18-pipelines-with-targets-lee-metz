1f2a3982987b66b6de476cfae7fd8f4450afe6c5
Fabian <78969880+FaMe-1337@users.noreply.github.com>
2022-11-15 22:03:38 +0100

    update

--- R/2-initial_targets.R
+++ R/2-initial_targets.R
@@ -15,7 +15,7 @@ list(
   tar_target(flights_file, "Data/nyc-flights.csv", format = "file"), 
   tar_target(flights_data, get_data(flights_file)),
   tar_target(run_lm, run_lmodel(data$arr_delay, data$dep_delay, data$origin, data = flights_data, n_var = 2),
-  tar_target(metrics, mod_metrics(run_lm))           
+  #tar_target(metrics, mod_metrics(run_lm))           
   #add your Target for the Plot here, dont't forget that the file has end in a list of targets seperated by a comma          
              
              )
