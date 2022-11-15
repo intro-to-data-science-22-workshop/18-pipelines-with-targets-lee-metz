a83e0d2982bde112d6877f684af250e1ba859465
Fabian <78969880+FaMe-1337@users.noreply.github.com>
2022-11-15 22:03:08 +0100

    implementing targets

--- R/2-initial_targets.R
+++ R/2-initial_targets.R
@@ -2,17 +2,24 @@ library(targets)
 source("R/1-functions.R")
 tar_option_set(
   packages = c(
-    "tidyverse"
+    "kableExtra",
+    "tidyverse",
+    "targets",
+    "nycflights13",
+    "stargazer"
+    
   )
 )
 
 list(
   tar_target(flights_file, "Data/nyc-flights.csv", format = "file"), 
-  tar_target(flights_data, load_data(flights_file)),
-  tar_target(run_lm, run_lmodel(data$arr_delay, data$dep_delay, data$carrier, data$distance, data = flights_data, n_var = 3))
+  tar_target(flights_data, get_data(flights_file)),
+  tar_target(run_lm, run_lmodel(data$arr_delay, data$dep_delay, data$origin, data = flights_data, n_var = 2),
+  tar_target(metrics, mod_metrics(run_lm))           
+  #add your Target for the Plot here, dont't forget that the file has end in a list of targets seperated by a comma          
+             
+             )
   
-  #tar_target(run_glm, rund_model(model= "glm",flights_data)),
-  #tar_target(report_results, rund_model(flights_data)) # I do not exactly remember how to establish a report as target
   
   )
 
